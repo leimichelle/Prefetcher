acadf9eb52b66748c52373e72ec106377cfe57be
michelleleiii <michellerp1013@gmail.com>
2017-11-19 15:37:34 -0500

    a better microbenchmark for the next-line prefetcher

--- mbq1.c
+++ mbq1.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-#define NSETS 128
-// compiled with -O0
+#define NSETS 32
+
+struct s {
+    int a;
+    int b;
+    int c;
+    int d;
+};
+// compiled 
 int main(int argc, char* argv[]) {
-    //In my configuration, I have a 64-set direct-mapped cache with an 8-byte cache line,
-    //so I would initiate an array of 128 integers (128*4=64*8)to avoid capacity miss 
-    int a[NSETS];
+    /*In my configuration, I have a 64-set direct-mapped cache with an 8-byte cache line*/
+    struct s a[NSETS];
     int i;
-    //If I write to the array sequentially, I should get an almost zero L1 miss rate because
-    //the prefetcher always prefetches the next cache-line.
+    /*Each struct accesses two cache-lines, and only accesses each once. Therefore, without a prefetcher, it will suffer from cache misses, because the same cache-line is never accessed more than once. With the next-line prefetcher, if I write to the array of struct s sequentially, I should get an almost 0% L1 miss rate because the prefetcher always prefetches the next cache-line.*/
     for (i=0; i<NSETS; i++) {
-        a[i] = i;    
+        a[i].a = i;
+        a[i].c = i;
     }
     return 0;
 }
