a95afe94d29d3f0909150e082a7a9fec82c71a7b
Lrac <carl.lam95@gmail.com>
2017-11-19 19:26:31 -0500

    update microbenchmarks

--- mbq1.c
+++ mbq1.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-#define NSETS 32
+#define NSETS 1000000
 
 struct s {
-    int a;
-    int b;
-    int c;
-    int d;
+  int a;
+  int b;
 };
 // compiled 
 int main(int argc, char* argv[]) {
-    /*In my configuration, I have a 64-set direct-mapped cache with an 8-byte cache line*/
+    /*In my configuration, I have a 128-set direct-mapped cache with an 8-byte cache line*/
     struct s a[NSETS];
     int i;
-    /*Each struct accesses two cache-lines, and only accesses each once. Therefore, without a prefetcher, it will suffer from cache misses, because the same cache-line is never accessed more than once. With the next-line prefetcher, if I write to the array of struct s sequentially, I should get an almost 0% L1 miss rate because the prefetcher always prefetches the next cache-line.*/
+    /*Each struct fits in one cache-line. Without a prefetcher, it will suffer 
+     * from cache misses, because the same cache-line is never accessed more than once.
+     * With the next-line prefetcher, if I write to the array of struct s sequentially,
+     * I should get an almost 0% L1 miss rate because the prefetcher always prefetches
+     * the next cache-line.*/
     for (i=0; i<NSETS; i++) {
         a[i].a = i;
-        a[i].c = i;
     }
     return 0;
 }
--- mbq2.c
+++ mbq2.c
@@ -7,15 +7,12 @@ struct s {
     int b;
     int c;
     int d;
-    int e;
-    int f;
-    int g;
-    int h;
 };
 // compiled 
 int main(int argc, char* argv[]) {
-    /*In my configuration, I have a 128-set direct-mapped cache with an 8-byte cache line*/
+    /*In my configuration, I have a 2-set direct-mapped cache with an 8-byte cache line*/
     struct s a[NSETS];
+    struct s b[NSETS];
     int i;
     /*Each iteration accesses a cache-line that is more than one cache-line apart from the previous iteration, so a next-line prefetcher would not work well for this certain memory access pattern. However, a stride prefetcher can capture the stride pattern to prefetch, which is more flexible than the next-line prefetcher in this sense. The stride prefetcher has a near 0 miss rate for the following access pattern.*/
     for (i=0; i<NSETS; i++) {
